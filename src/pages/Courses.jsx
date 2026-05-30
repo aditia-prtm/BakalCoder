d880480daa307686d460b71ca80bfba29f16723b
M. Aditia Putra Pratama <mhdaditiapratama268@gmail.com>
2026-05-30 15:24:09 +0700

    feat: update Courses page

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -6,19 +6,22 @@ export default function Courses() {
       <h2 className="text-3xl font-bold text-amber-500 border-b border-slate-800 pb-3">Daftar Silabus Kelas</h2>
       <div className="grid md:grid-cols-3 gap-6">
         <div className="bg-slate-900 border border-slate-800 p-6 rounded-xl hover:scale-105 duration-300 cursor-pointer">
-          <div className="text-cyan-400 font-mono text-sm mb-2">LEVEL 1</div>
-          <h3 className="text-xl font-bold mb-3 text-amber-500">Basic & Complexity</h3>
-          <p className="text-amber-50 text-sm mb-4">Pengenalan C++, standard library (STL), analisis waktu (Big O), manipulasi array dan string.</p>
+          <div className="text-cyan-400 font-mono text-sm mb-2">FUNDAMENTAL</div>
+          <h3 className="text-xl font-bold mb-3 text-amber-500">Struktur Data Linear & Notasi Big O</h3>
+          <p className="text-amber-50 text-sm mb-4">Pelajari cara penyimpanan data berurutan di memori serta dasar pengukuran efisiensi kode program menggunakan Notasi Big O</p>
+          <h5 className="text-sm text-cyan-300 font-semibold"><span className="font-bold">Materi : </span>Array, Linked List, Stack, Queue</h5>
         </div>
         <div className="bg-slate-900 border border-slate-800 p-6 rounded-xl hover:scale-105 duration-300 cursor-pointer">
-          <div className="text-cyan-400 font-mono text-sm mb-2">LEVEL 2</div>
-          <h3 className="text-xl font-bold mb-3 text-amber-500">Fundamental Techniques</h3>
-          <p className="text-amber-50 text-sm mb-4">Penerapan teknik Greedy, Brute Force yang efisien, Binary Search, Sorting, dan konsep Matematika Dasar CP.</p>
+          <div className="text-cyan-400 font-mono text-sm mb-2">INTERMEDIATE</div>
+          <h3 className="text-xl font-bold mb-3 text-amber-500">Struktur Non-Linear & Pencarian</h3>
+          <p className="text-amber-50 text-sm mb-4">Pahami representasi data hierarkis dan bercabang untuk efisiensi pencarian data serta implementasi berbagai metode sorting</p>
+          <h5 className="text-sm text-cyan-300 font-semibold"><span className="font-bold">Materi : </span>Tree, BST, Hashing, Merge/Quick Sort</h5>
         </div>
         <div className="bg-slate-900 border border-slate-800 p-6 rounded-xl hover:scale-105 duration-300 cursor-pointer">
-          <div className="text-cyan-400 font-mono text-sm mb-2">LEVEL 3</div>
-          <h3 className="text-xl font-bold mb-3 text-amber-500">Advanced Structures</h3>
-          <p className="text-amber-50 text-sm mb-4">Pendalaman Graph (DFS/BFS), Tree, Dynamic Programming (DP) dasar, hingga Segment Tree.</p>
+          <div className="text-cyan-400 font-mono text-sm mb-2">ADVANCE</div>
+          <h3 className="text-xl font-bold mb-3 text-amber-500">Graf & Teknik Optimasi</h3>
+          <p className="text-amber-50 text-sm mb-4">Pelajari pemodelan jaringan, pencarian rute terpendek, dan teknik pemecahan masalah kompleks secara optimal</p>
+          <h5 className="text-sm text-cyan-300 font-semibold"><span className="font-bold">Materi : </span>Graph (BFS/DFS), Dijkstra, Dynamic Programming</h5>
         </div>
       </div>
     </div>
--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 export default function Leaderboard() {
   const dataUser = [
     {rank: 1, nama: "Khvicha Kvaratskhelia", institusi: "Paris Saint-Germain", poin: 300},
-    {rank: 2, nama: "Ousmane Dembele", institusi: "Paris Saint-Germain", poin: 286},
-    {rank: 3, nama: "Harry Kane", institusi: "FC Bayern Munchen", poin: 261},
+    {rank: 2, nama: "Ousmane Dembélé", institusi: "Paris Saint-Germain", poin: 286},
+    {rank: 3, nama: "Harry Kane", institusi: "FC Bayern München", poin: 261},
     {rank: 4, nama: "Declan Rice", institusi: "Arsenal", poin: 215},
-    {rank: 5, nama: "Michael Olise", institusi: "FC Bayern Munchen", poin: 196},
+    {rank: 5, nama: "Michael Olise", institusi: "FC Bayern München", poin: 196},
     {rank: 6, nama: "Lamine Yamal", institusi: "FC Barcelona", poin: 172},
     {rank: 7, nama: "Vitinha", institusi: "Paris Saint-Germain", poin: 127},
-    {rank: 8, nama: "Kylian Mbappe", institusi: "Real Madrid", poin: 125},
-    {rank: 9, nama: "Bruno Fernandes", institusi: "Manhester United", poin: 96},
-    {rank: 10, nama: "Erling Haaland", institusi: "Manhester City", poin: 90}
+    {rank: 8, nama: "Kylian Mbappé", institusi: "Real Madrid", poin: 125},
+    {rank: 9, nama: "Bruno Fernandes", institusi: "Manchester United", poin: 96},
+    {rank: 10, nama: "Erling Haaland", institusi: "Manchester City", poin: 90}
   ];
 
   return (
     <div className="space-y-6">
       <h2 className="text-3xl font-bold text-amber-500 border-b border-slate-800 pb-3">Leaderboard</h2>
       <p className="italic text-amber-400">Ayooo!... Jangan mau ketinggalan sama temen kamu</p>
-       <div className="bg-slate-900 border border-slate-800 rounded-xl overflow-hidden">
+      <div className="bg-slate-900 border border-slate-800 rounded-xl overflow-hidden">
         <table className="w-full text-left border-collapse">
           <thead>
             <tr className="bg-slate-800/50 text-amber-500 text-sm border-b border-slate-800">
@@ -43,6 +43,7 @@ export default function Leaderboard() {
           </tbody>
         </table>
       </div>
+      <p className="italic text-amber-400 px-5 flex justify-center">Note: Peringkat diatas adalah prediksi saya terhadap hasil Ballon d'Or 2026</p>      
     </div>
   );
-}
+} 
