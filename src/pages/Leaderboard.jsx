3eb7df1566bdc9662d96947d2318bc44d040ceac
M. Aditia Putra Pratama <mhdaditiapratama268@gmail.com>
2026-06-01 21:40:41 +0700

    chore: update Leaderboard stats

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -4,14 +4,14 @@ export default function Leaderboard() {
   const dataUser = [
     {rank: 1, nama: "Khvicha Kvaratskhelia", institusi: "Paris Saint-Germain", poin: 300},
     {rank: 2, nama: "Ousmane Dembélé", institusi: "Paris Saint-Germain", poin: 286},
-    {rank: 3, nama: "Harry Kane", institusi: "FC Bayern München", poin: 261},
-    {rank: 4, nama: "Declan Rice", institusi: "Arsenal", poin: 215},
-    {rank: 5, nama: "Michael Olise", institusi: "FC Bayern München", poin: 196},
-    {rank: 6, nama: "Lamine Yamal", institusi: "FC Barcelona", poin: 172},
-    {rank: 7, nama: "Vitinha", institusi: "Paris Saint-Germain", poin: 127},
+    {rank: 3, nama: "Declan Rice", institusi: "Arsenal", poin: 241},
+    {rank: 4, nama: "Michael Olise", institusi: "FC Bayern München", poin: 215},
+    {rank: 5, nama: "Vitinha", institusi: "Paris Saint-Germain", poin: 212},
+    {rank: 6, nama: "Harry Kane", institusi: "FC Bayern München", poin: 188},
+    {rank: 7, nama: "Lamine Yamal", institusi: "FC Barcelona", poin: 172},
     {rank: 8, nama: "Kylian Mbappé", institusi: "Real Madrid", poin: 125},
-    {rank: 9, nama: "Bruno Fernandes", institusi: "Manchester United", poin: 96},
-    {rank: 10, nama: "Erling Haaland", institusi: "Manchester City", poin: 90}
+    {rank: 9, nama: "João Neves", institusi: "Paris Saint-Germain", poin: 112},
+    {rank: 10, nama: "Bruno Fernandes", institusi: "Manchester United", poin: 96}
   ];
 
   return (
