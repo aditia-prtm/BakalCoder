1ef95e7155bb93eb847ec462e6d91baffdbbe988
M. Aditia Putra Pratama <mhdaditiapratama268@gmail.com>
2026-06-05 18:42:20 +0700

    feat: add new navbar fitur & effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { Menu, Monitor } from 'lucide-react';
+import { Menu } from 'lucide-react';
 import { useState } from 'react';
 
 export default function Navbar({ user, onLogout, activeTab, setActiveTab }) {
   const [mobileNav, setMobileMav] = useState(false);
+
+  const moveTo = (nextTab) => {
+    setActiveTab(nextTab);
+    setMobileMav(false);
+  };
+
   return (
     <>
       {user && (
@@ -19,19 +25,19 @@ export default function Navbar({ user, onLogout, activeTab, setActiveTab }) {
 
             <div className="hidden md:flex items-center gap-3 sm:gap-5 md:gap-8 lg:gap-12">
               <button 
-                onClick={() => setActiveTab('courses')} 
+                onClick={() => moveTo('courses')} 
                 className={`hover:text-cyan-400 transition duration-300 cursor-pointer ${activeTab === 'courses' ? 'text-cyan-400 font-semibold' : 'text-slate-300'}`}
               >
                 COURSES
               </button>
               <button 
-                onClick={() => setActiveTab('problems')} 
+                onClick={() => moveTo('problems')} 
                 className={`hover:text-cyan-400 transition duration-300 cursor-pointer ${activeTab === 'problems' ? 'text-cyan-400 font-semibold' : 'text-slate-300'}`}
               >
                 PROBLEM
               </button>
               <button 
-                onClick={() => setActiveTab('leaderboard')} 
+                onClick={() => moveTo('leaderboard')} 
                 className={`hover:text-cyan-400 transition duration-300 cursor-pointer ${activeTab === 'leaderboard' ? 'text-cyan-400 font-semibold' : 'text-slate-300'}`}
               >
                 LEADERBOARD
@@ -49,28 +55,29 @@ export default function Navbar({ user, onLogout, activeTab, setActiveTab }) {
             </div>
 
             <div className='flex md:hidden'>
-              <Menu onClick={() => setMobileMav(!mobileNav)} className={`${mobileNav ? 'text-cyan-400' : 'text-white'}`}/>
+              <Menu onClick={() => setMobileMav(!mobileNav)} className={`${mobileNav ? 'text-cyan-400' : 'text-white'} duration-300`}/>
             </div>
           </nav>
    
           <div className='flex justify-end'>
-            {mobileNav && (
-              <div className="fixed flex md:hidden flex-col items-center rounded-xs border border-slate-800 border-t-0 w-3/7 py-5 bg-slate-900 gap-7">
+            {mobileNav && ( 
+              <div className={`flex md:hidden flex-col items-center rounded-xs border border-slate-800 border-t-0 w-1/3 sm:w-1/4 py-3 bg-slate-900 gap-7 
+                              [animation:slideLeft_0.3s_ease-out]`}>
                 <span className="w-full text-md pl-3 border-b pb-3 border-slate-700 font-bold text-slate-400">Halo, <strong className="text-amber-500">{user}</strong></span>
                 <button 
-                  onClick={() => setActiveTab('courses')} 
+                  onClick={() => moveTo('courses')} 
                   className={`hover:text-cyan-400 transition duration-300 cursor-pointer w-full  text-left pl-3 ${activeTab === 'courses' ? 'text-cyan-400 font-semibold' : 'text-slate-300'}`}
                 >
                   COURSES
                 </button>
                 <button 
-                  onClick={() => setActiveTab('problems')} 
+                  onClick={() => moveTo('problems')} 
                   className={`hover:text-cyan-400 transition duration-300 cursor-pointer  w-full text-left pl-3 ${activeTab === 'problems' ? 'text-cyan-400 font-semibold' : 'text-slate-300'}`}
                 >
                   PROBLEM
                 </button>
                 <button 
-                  onClick={() => setActiveTab('leaderboard')} 
+                  onClick={() => moveTo('leaderboard')} 
                   className={`hover:text-cyan-400 transition duration-300 cursor-pointer w-full text-left pl-3 ${activeTab === 'leaderboard' ? 'text-cyan-400 font-semibold' : 'text-slate-300'}`}
                 >
                   LEADERBOARD
